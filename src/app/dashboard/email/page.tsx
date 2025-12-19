986fff35e587e869792b4db664f34e089cba2f34
Abdel Majid Zahmoul <Majdi.amz@gmail.com>
2025-12-19 21:56:41 +0000

    in the second column, reduce the email content preview to the first 35 c

--- src/app/dashboard/email/page.tsx
+++ src/app/dashboard/email/page.tsx
@@ -53,7 +53,7 @@ const EmailList = ({
         <div className="p-2 space-y-1">
           {emails.map(email => {
             const plainBody = email.body.replace(/<[^>]*>?/gm, ' ');
-            const snippet = plainBody.length > 50 ? `${plainBody.substring(0, 50)}...` : plainBody;
+            const snippet = plainBody.length > 35 ? `${plainBody.substring(0, 35)}...` : plainBody;
             return (
               <button
                 key={email.id}
